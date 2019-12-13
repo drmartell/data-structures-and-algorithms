ec0c9e3b770f05a516f130e09b21537f78ed45e5
Casey Martell <drmartell@gmail.com>
2019-12-12 21:36:40 -0800

    further refactoring

--- challenges/linkedList/ll-merge.js
+++ challenges/linkedList/ll-merge.js
@@ -1,30 +1,22 @@
 const LinkedList = require('../linkedList/linked-list');
 
 const mergeLists = (ll1, ll2) => {
-  const
-    merged = new LinkedList(),
-    currentObj = {};
+  const merged = new LinkedList();
+  const currentObj = { current1: ll1.head.next, current2: ll2.head };
   merged.head = ll1.head;
   merged.length = 1;
-  let
-    current1 = ll1.head.next,
-    current2 = ll2.head,
-    currentMerged = merged.head;
-  const
-    merge = currentString => {
-      const current = currentObj[currentString];
-      currentMerged.next = current;
-      currentMerged = current;
-      currentObj[currentString] = current.next;
-      ++merged.length;
-    };
-  currentObj['current1'] = current1;
-  currentObj['current2'] = current2;
-  while(currentObj['current2']) {
+  let currentMerged = merged.head;
+  const merge = currentString => {
+    currentMerged.next = currentObj[currentString];
+    currentMerged = currentObj[currentString];
+    currentObj[currentString] = currentObj[currentString].next;
+    ++merged.length;
+  };
+  while(currentObj.current2) {
     merge('current2');
-    if(currentObj['current1']) merge('current1');
+    if(currentObj.current1) merge('current1');
   }
-  while(currentObj['current1']) merge('current1');
+  while(currentObj.current1) merge('current1');
 
   return merged;
 };
